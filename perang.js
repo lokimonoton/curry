a5d0e8d337fd8a8bcb91c67b954b285e21bcc10d
sampan <slafe1@yandex.com>
2017-07-08 03:41:03 +0000

    menambah chrome linux sebagai downloader chrome-linux dan juga menambah perang.js dengan lokasi chrome linux

--- perang.js
+++ perang.js
@@ -516,11 +516,13 @@ function masukC9(projectName){
    var webdriver = require('selenium-webdriver'),
       By = webdriver.By,
       until = webdriver.until;
+var chrome = require("selenium-webdriver/chrome");
 
-  var driver = new webdriver.Builder()
-      .forBrowser('chrome')
-      // .usingServer('http://localhost:4444/wd/hub')
-      .build();
+var options = new chrome.Options();
+options.setChromeBinaryPath("chrome-linux/chrome");
+options.addArguments(["--no-sandbox"]);
+var driver = new webdriver.Builder().
+withCapabilities(options.toCapabilities()).build();
   driver.manage().window().setSize(1000, 1000);
   driver.get('https://www.c9.io/login');
   driver.findElement(By.id('id-username')).sendKeys("slafe1@yandex.com");
